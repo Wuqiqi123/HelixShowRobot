2330b3ec71a61cab2c408a1bdb0575bce759d9c7
wuqiqi <1258552199@qq.com>
2019-03-26 17:54:03 +0800

    移植别人的程序到自己的程序中，主要是初始化界面配置

--- HelixSCARA/HelixSCARA/MainWindow.xaml.cs
+++ HelixSCARA/HelixSCARA/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelixToolkit.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,19 +11,149 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HelixSCARA
 {
+    class Joint
+    {
+        public Model3D model = null;
+        public double angle = 0;
+        public double angleMin = -180;
+        public double angleMax = 180;
+        public int rotPointX = 0;
+        public int rotPointY = 0;
+        public int rotPointZ = 0;
+        public int rotAxisX = 0;
+        public int rotAxisY = 0;
+        public int rotAxisZ = 0;
+
+        public Joint(Model3D pModel)
+        {
+            model = pModel;
+        }
+    }
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        Model3DGroup RA = new Model3DGroup(); //RoboticArm 3d group
+        Model3D geom = null; //Debug sphere to check in which point the joint is rotatin
+        ModelVisual3D visual;
+        ModelVisual3D RoboticArm = new ModelVisual3D();
+        GeometryModel3D oldSelectedModel = null;
+        Color oldColor = Colors.White;
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+            var builder = new MeshBuilder(true, true);
+            var position = new Point3D(0, 0, 0);
+            builder.AddSphere(position, 50, 15, 15);
+            geom = new GeometryModel3D(builder.ToMesh(), Materials.Brown);
+            visual = new ModelVisual3D();
+            visual.Content = geom;
+            viewPort3d.RotateGesture = new MouseGesture(MouseAction.RightClick);
+            viewPort3d.PanGesture = new MouseGesture(MouseAction.LeftClick);
+            viewPort3d.Children.Add(visual);
+            viewPort3d.Children.Add(RoboticArm);
+            viewPort3d.Camera.LookDirection = new Vector3D(2038, -5200, -2930);
+            viewPort3d.Camera.UpDirection = new Vector3D(-0.145, 0.372, 0.917);
+            viewPort3d.Camera.Position = new Point3D(-1571, 4801, 3774);
         }
+        private void ViewPort3D_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Point mousePos = e.GetPosition(viewPort3d);
+            PointHitTestParameters hitParams = new PointHitTestParameters(mousePos);
+            VisualTreeHelper.HitTest(viewPort3d, null, ResultCallback, hitParams);
+        }
+
+        private void ViewPort3D_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // Perform the hit test on the mouse's position relative to the viewport.
+            HitTestResult result = VisualTreeHelper.HitTest(viewPort3d, e.GetPosition(viewPort3d));
+            RayMeshGeometry3DHitTestResult mesh_result = result as RayMeshGeometry3DHitTestResult;
+
+            if (oldSelectedModel != null)
+                unselectModel();
+
+            if (mesh_result != null)
+            {
+                selectModel(mesh_result.ModelHit);
+            }
+        }
+        public HitTestResultBehavior ResultCallback(HitTestResult result)
+        {
+            // Did we hit 3D?
+            RayHitTestResult rayResult = result as RayHitTestResult;
+            if (rayResult != null)
+            {
+                // Did we hit a MeshGeometry3D?
+                RayMeshGeometry3DHitTestResult rayMeshResult = rayResult as RayMeshGeometry3DHitTestResult;
+                geom.Transform = new TranslateTransform3D(new Vector3D(rayResult.PointHit.X, rayResult.PointHit.Y, rayResult.PointHit.Z));
+
+                if (rayMeshResult != null)
+                {
+                    // Yes we did!
+                }
+            }
+
+            return HitTestResultBehavior.Continue;
+        }
+        private void unselectModel()
+        {
+            changeModelColor(oldSelectedModel, oldColor);
+        }
+
+        private Color changeModelColor(GeometryModel3D pModel, Color newColor)
+        {
+            if (pModel == null)
+                return oldColor;
+
+            Color previousColor = Colors.Black;
+
+            MaterialGroup mg = (MaterialGroup)pModel.Material;
+            if (mg.Children.Count > 0)
+            {
+                try
+                {
+                    previousColor = ((EmissiveMaterial)mg.Children[0]).Color;
+                    ((EmissiveMaterial)mg.Children[0]).Color = newColor;
+                    ((DiffuseMaterial)mg.Children[1]).Color = newColor;
+                }
+                catch (Exception exc)
+                {
+                    previousColor = oldColor;
+                }
+            }
+
+            return previousColor;
+        }
+
+        private Color changeModelColor(Joint pJoint, Color newColor)
+        {
+            Model3DGroup models = ((Model3DGroup)pJoint.model);
+            return changeModelColor(models.Children[0] as GeometryModel3D, newColor);
+        }
+
+        private void selectModel(Model3D pModel)
+        {
+            try
+            {
+                Model3DGroup models = ((Model3DGroup)pModel);
+                oldSelectedModel = models.Children[0] as GeometryModel3D;
+            }
+            catch (Exception exc)
+            {
+                oldSelectedModel = (GeometryModel3D)pModel;
+            }
+            oldColor = changeModelColor(oldSelectedModel, ColorHelper.HexToColor("#ff3333"));
+        }
+
     }
 }
